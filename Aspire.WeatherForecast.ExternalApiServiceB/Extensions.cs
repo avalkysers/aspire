8b70cbf3159c042270267195cc28ebbcea55d1c3
Andreas Valkysers <andreas.valkysers@milestone-consult.de>
2024-11-19 08:51:30 +0100

    Add switch for service defaults to ExternalApiServiceB

--- Aspire.WeatherForecast.ExternalApiServiceB/Extensions.cs
+++ Aspire.WeatherForecast.ExternalApiServiceB/Extensions.cs
@@ -4,8 +4,18 @@ public static class Extensions
 {
     public static IHostApplicationBuilder AddServices(this IHostApplicationBuilder builder)
     {
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        // Load global settings from apphost for show-casing...
+        builder.Configuration.AddJsonFile(Path.Combine(AppContext.BaseDirectory, "aspirehosting.appsettings.json"), optional: false);
+
+        if (builder.Configuration["ASPIRE:ADD_SERVICE_DEFAULTS_TO_EXTERNAL_A"] == "true")
+        {
+            builder.AddServiceDefaults();
+        }
+        else
+        {
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+        }
 
         return builder;
     }
